231f8153720d1d093637e7185236cbee5e3380bb
GabsBaquie <117845004+GabsBaquie@users.noreply.github.com>
2025-05-13 18:10:14 +0200

    Mise à jour des dépendances, modification de l'URL de connexion socket et ajout de nouvelles configurations dans app.json

--- core/api/session.api.tsx
+++ core/api/session.api.tsx
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const Socket = io("http://10.33.70.48:3000", {
+export const Socket = io("http://10.33.70.48:3001", {
   transports: ["websocket"],
   reconnectionAttempts: 5,
   reconnectionDelay: 1000,
