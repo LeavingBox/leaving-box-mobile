2b86bee150cd33dde9cd465b05f7224d5529ed9b
unknown <guesdon.samuelpro@gmail.com>
2026-03-04 09:49:42 +0100

    changed all names

---
+++ app/analyst/joinGame.tsx
@@ -0,0 +1,142 @@
+import React, { useState } from "react";
+import { View, Text, TextInput, StyleSheet, Alert } from "react-native";
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { LinearGradient } from "expo-linear-gradient";
+import NavigationButton from "@/components/NavigationButton";
+import { Socket } from "@/core/api/session.api";
+import { useRouter } from "expo-router";
+
+export default function JoinGame() {
+  const router = useRouter();
+  const [code, setCode] = useState("");
+
+  const joinGame = () => {
+    Socket.connect();
+    Socket.emit(
+      "getSession",
+      { sessionCode: code },
+      (response: { success: boolean; message?: string }) => {
+        if (response.success) {
+          Socket.emit("joinSession", {
+            sessionCode: code,
+            player: "analyste",
+          });
+          Socket.on("playerJoined", () => {
+            console.log("playerJoined");
+            Socket.off("playerJoined");
+            router.navigate({
+              pathname: "/agent/waitingRoom",
+              params: { sessionCode: code, role: "analyste" },
+            });
+          });
+        } else {
+          Alert.alert("Error while joining session", response.message);
+        }
+      },
+    );
+  };
+
+  const handleBack = () => {
+    Socket.off("playerJoined");
+    Socket.off("currentSession");
+    Socket.emit("leaveSession", { sessionCode: code, player: "analyste" });
+    router.navigate("/");
+  };
+
+  const handleNext = () => {
+    if (code.length > 0) {
+      joinGame();
+    }
+  };
+  return (
+    <ParallaxScrollView>
+      <View style={styles.container}>
+        <Text style={styles.title}>Why do we use it?</Text>
+
+        <LinearGradient
+          colors={["#66a6ff", "#89f7fe"]}
+          start={{ x: 0, y: 0 }}
+          end={{ x: 1, y: 1 }}
+          style={styles.inputContainer}
+        >
+          <TextInput
+            style={styles.input}
+            placeholder="Enter code"
+            placeholderTextColor="#000000"
+            value={code}
+            onChangeText={setCode}
+          />
+        </LinearGradient>
+
+        <Text style={styles.description}>
+          Proident est dolore ullamco cupidatat non ullamco anim. Laborum ea
+          aliquip magna deserunt qui. Elit mollit elit deserunt velit labore
+          proident adipisicing nisi esse sunt laboris. Magna eu dolore ad. Aute
+          Lorem aute tempor dolore nisi aliqua reprehenderit commodo ut laborum
+          nostrud laboris pariatur. Duis amet in minim sunt amet adipisicing
+          velit consectetur amet pariatur sunt ut.
+        </Text>
+        <View style={styles.navigationContainer}>
+          <NavigationButton
+            label="Retour"
+            onPress={handleBack}
+            color="blue"
+            textColor="black"
+          />
+          {code.length > 0 && (
+            <NavigationButton
+              label="Rejoindre la partie"
+              color="blue"
+              onPress={handleNext}
+            />
+          )}
+        </View>
+      </View>
+    </ParallaxScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    alignItems: "center",
+    marginVertical: 50,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "white",
+    textAlign: "center",
+    marginVertical: 10,
+  },
+  description: {
+    fontSize: 14,
+    color: "white",
+    textAlign: "center",
+    marginBottom: 50,
+    marginTop: 20,
+    paddingHorizontal: 15,
+  },
+  inputContainer: {
+    width: "80%",
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    alignSelf: "center",
+    marginVertical: 20,
+  },
+  input: {
+    color: "#000000",
+    fontSize: 16,
+    textAlign: "center",
+    borderRadius: 10,
+  },
+  navigationContainer: {
+    marginTop: 30,
+    flexDirection: "row",
+    justifyContent: "center",
+    gap: 20,
+    padding: 20,
+  },
+});
---
+++ app/analyst/manual.tsx
@@ -0,0 +1,150 @@
+import ManualsNav from "@/components/manual/ManualsNav";
+import ModuleInstructions from "@/components/manual/ModuleInstructions";
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { Socket } from "@/core/api/session.api";
+import { ModuleManual } from "@/core/interface/module.interface";
+import { useLocalSearchParams, useRouter } from "expo-router";
+import { useEffect, useState } from "react";
+import {
+  Alert,
+  Dimensions,
+  FlatList,
+  Image,
+  ImageBackground,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import { WebView } from "react-native-webview";
+
+const { width } = Dimensions.get("window");
+
+export default function Manual() {
+  const router = useRouter();
+  const { sessionCode, maxTime, role, moduleManuals } = useLocalSearchParams();
+  const [selectedManual, setSelectedManual] = useState<ModuleManual | null>(
+    null,
+  );
+  const Manuals: ModuleManual[] = JSON.parse(moduleManuals as string);
+
+  useEffect(() => {
+    const handleSessionCleared = (res: any) => {
+      Alert.alert(
+        "Fermeture de la session",
+        "L'agent hôte de la session a quitté la salle d'attente. La session va être fermée.",
+      );
+      handleDisconnected();
+    };
+
+    Socket.on("sessionCleared", handleSessionCleared);
+    Socket.on("gameOver", (data: any) => {
+      Alert.alert("Fin de la partie", data.message, [
+        { text: "MENU", onPress: () => handleDisconnected() },
+      ]);
+    });
+
+    return () => {
+      Socket.off("sessionCleared", handleSessionCleared);
+      Socket.on("gameOver", (data: any) => {
+        Alert.alert("Fin de la partie", data.message, [
+          { text: "MENU", onPress: () => handleDisconnected() },
+        ]);
+      });
+    };
+  }, []);
+
+  const handleDisconnected = () => {
+    Socket.disconnect();
+    router.navigate({
+      pathname: "/analyst/joinGame",
+    });
+  };
+
+  return (
+    <ParallaxScrollView>
+      <View style={styles.mainContainer}>
+        <View style={styles.navContainer}>
+          <ScrollView>
+            {Manuals.map((manual, index) => (
+              <ManualsNav
+                key={index}
+                index={index}
+                manual={manual}
+                selectedManual={selectedManual}
+                length={Manuals.length}
+                setSelectedManual={(manual: ModuleManual) => {
+                  setSelectedManual(manual);
+                }}
+              />
+            ))}
+          </ScrollView>
+        </View>
+
+        <ImageBackground
+          source={require("../../assets/images/folder_background.png")}
+          style={styles.folderBackground}
+          resizeMode="repeat"
+        >
+          <Image
+            source={require("../../assets/images/paperclip.png")}
+            style={styles.paperclip}
+          />
+
+          <View style={styles.contentContainer}>
+            {selectedManual ? (
+              <ModuleInstructions manual={selectedManual} />
+            ) : (
+              <Text style={styles.title}>
+                Bomb Defusal Manual, for an analyst
+              </Text>
+            )}
+          </View>
+        </ImageBackground>
+      </View>
+    </ParallaxScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    alignItems: "flex-start",
+    flex: 1,
+    marginVertical: 60,
+  },
+  navContainer: {
+    flexDirection: "column",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginTop: 20,
+    marginVertical: 100,
+  },
+  folderBackground: {
+    zIndex: 10,
+    width: width * 0.91,
+    minHeight: 650,
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    justifyContent: "flex-start",
+    boxShadow: "1px 5px 3px 5px rgba(0, 0, 0, 0.50)",
+  },
+  paperclip: {
+    position: "absolute",
+    zIndex: 1,
+    top: -20,
+    right: 10,
+    width: 100,
+    height: 100,
+    transform: [{ rotate: "12deg" }],
+    resizeMode: "contain",
+  },
+  contentContainer: {
+    backgroundColor: "white",
+    padding: 15,
+  },
+});
